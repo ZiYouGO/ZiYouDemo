bdb1ca1c9f879148fb8b8b482975ff12505cfddd
stillbear <18787460460@163.com>
2016-04-08 14:25:12 +0800

    Gui-Demo v0.2
    Sheet achieved

---
+++ GUI-Demo/app/src/main/java/com/mingle/ZiYou/Content/MapActivity.java
@@ -0,0 +1,171 @@
+package com.mingle.ZiYou.Content;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.mingle.entity.MenuEntity;
+import com.mingle.myapplication.R;
+import com.mingle.sweetpick.BlurEffect;
+import com.mingle.sweetpick.CustomDelegate;
+import com.mingle.sweetpick.DimEffect;
+import com.mingle.sweetpick.RecyclerViewDelegate;
+import com.mingle.sweetpick.SweetSheet;
+import com.mingle.sweetpick.ViewPagerDelegate;
+
+import java.util.ArrayList;
+
+public class MapActivity extends AppCompatActivity {
+
+    private SweetSheet mSweetSheet;
+    private SweetSheet mSweetSheet2;
+    private SweetSheet mSweetSheet3;
+    private RelativeLayout rl;
+    Button Sheet;
+    Button Back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_map);
+
+        rl = (RelativeLayout) findViewById(R.id.rl);
+
+        Back=(Button)findViewById(R.id.Back);
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MapActivity.this.finish();
+            }
+        });
+
+        Sheet=(Button)findViewById(R.id.Sheet);
+        Sheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSweetSheet.toggle();
+            }
+        });
+
+        setupViewpager();
+        setupRecyclerView();
+        setupCustomView();
+
+    }
+
+    private void setupCustomView() {
+
+        mSweetSheet3 = new SweetSheet(rl);
+        CustomDelegate customDelegate = new CustomDelegate(true,
+                CustomDelegate.AnimationType.DuangLayoutAnimation);
+        View view = LayoutInflater.from(this).inflate(R.layout.layout_custom_view, null, false);
+        customDelegate.setCustomView(view);
+        mSweetSheet3.setDelegate(customDelegate);
+        view.findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSweetSheet3.dismiss();
+            }
+        });
+    }
+
+    private void setupRecyclerView() {
+
+        final ArrayList<MenuEntity> list = new ArrayList<>();
+        //添加假数据
+        MenuEntity menuEntity1 = new MenuEntity();
+        menuEntity1.iconId = R.drawable.ic_account_child;
+        menuEntity1.titleColor = 0xff000000;
+        menuEntity1.title = "code";
+        MenuEntity menuEntity = new MenuEntity();
+        menuEntity.iconId = R.drawable.ic_account_child;
+        menuEntity.titleColor = 0xffb3b3b3;
+        menuEntity.title = "QQ";
+        list.add(menuEntity1);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        // SweetSheet 控件,根据 rl 确认位置
+        mSweetSheet = new SweetSheet(rl);
+
+        //设置数据源 (数据源支持设置 list 数组,也支持从菜单中获取)
+        mSweetSheet.setMenuList(list);
+        //根据设置不同的 Delegate 来显示不同的风格.
+        mSweetSheet.setDelegate(new RecyclerViewDelegate(true));
+        //根据设置不同Effect 来显示背景效果BlurEffect:模糊效果.DimEffect 变暗效果
+        mSweetSheet.setBackgroundEffect(new BlurEffect(8));
+        //设置点击事件
+        mSweetSheet.setOnMenuItemClickListener(new SweetSheet.OnMenuItemClickListener() {
+            @Override
+            public boolean onItemClick(int position, MenuEntity menuEntity1) {
+                //即时改变当前项的颜色
+                list.get(position).titleColor = 0xff5823ff;
+                ((RecyclerViewDelegate) mSweetSheet.getDelegate()).notifyDataSetChanged();
+
+                //根据返回值, true 会关闭 SweetSheet ,false 则不会.
+                Toast.makeText(MapActivity.this, menuEntity1.title + "  " + position, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+
+    }
+
+    private void setupViewpager() {
+
+
+        mSweetSheet2 = new SweetSheet(rl);
+
+        //从menu 中设置数据源
+        mSweetSheet2.setMenuList(R.menu.menu_sweet);
+        mSweetSheet2.setDelegate(new ViewPagerDelegate());
+        mSweetSheet2.setBackgroundEffect(new DimEffect(0.5f));
+        mSweetSheet2.setOnMenuItemClickListener(new SweetSheet.OnMenuItemClickListener() {
+            @Override
+            public boolean onItemClick(int position, MenuEntity menuEntity1) {
+
+                Toast.makeText(MapActivity.this, menuEntity1.title + "  " + position, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (mSweetSheet.isShow() || mSweetSheet2.isShow()) {
+            if (mSweetSheet.isShow()) {
+                mSweetSheet.dismiss();
+            }
+            if (mSweetSheet2.isShow()) {
+                mSweetSheet2.dismiss();
+            }
+        } else {
+            super.onBackPressed();
+        }
+
+
+    }
+}
---
+++ GUI-Demo/app/src/main/java/com/mingle/ZiYou/Main/MainActivity.java
@@ -0,0 +1,133 @@
+package com.mingle.ZiYou.Main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.mingle.ZiYou.Adapter.MyAdapter;
+import com.mingle.ZiYou.Content.MapActivity;
+import com.mingle.myapplication.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ListView listView=(ListView)findViewById(R.id.list);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        setSupportActionBar(toolbar);
+
+        List<Map<String, Object>> list=getData();
+        listView.setAdapter(new MyAdapter(this, list));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        drawer.setDrawerListener(toggle);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public List<Map<String, Object>> getData(){
+        List<Map<String, Object>> list=new ArrayList<Map<String,Object>>();
+        for (int i = 0; i < 10; i++) {
+            Map<String, Object> map=new HashMap<String, Object>();
+            map.put("image", R.drawable.demo_list);
+            map.put("title", "title"+i);
+            map.put("info", "information"+i);
+            list.add(map);
+        }
+        return list;
+    }
+}
