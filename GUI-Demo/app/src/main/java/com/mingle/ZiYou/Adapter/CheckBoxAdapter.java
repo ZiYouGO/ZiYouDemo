0c7d309409b5fb1873292f06d56746c0873c7366
Sunnyalicecai069960 <18813051642@163.com>
2016-05-23 19:46:12 +0800

    改名字

--- GUI-Demo/app/src/main/java/com/mingle/ZiYou/Adapter/CheckBoxAdapter.java
+++ GUI-Demo/app/src/main/java/com/mingle/ZiYou/Adapter/CheckBoxAdapter.java
@@ -1,4 +1,4 @@
-package com.mingle.ZiYou.Adapter;
+package com.mingle.ZiYou.adapter;
 
 import android.content.Context;
 import android.view.LayoutInflater;
--- GUI-Demo/app/src/main/java/com/mingle/ZiYou/Content/MapActivity.java
+++ GUI-Demo/app/src/main/java/com/mingle/ZiYou/Content/MapActivity.java
@@ -1,4 +1,4 @@
-package com.mingle.ZiYou.Content;
+package com.mingle.ZiYou.content;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
