baa29e45c8366824c478ef489bb0dd3e500f59fb
Sunnyalicecai069960 <18813051642@163.com>
2016-05-26 15:23:11 +0800

    。。

--- GUI-Demo/app/src/main/java/com/mingle/ZiYou/Main/MainActivity.java
+++ GUI-Demo/app/src/main/java/com/mingle/ZiYou/Main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mingle.ZiYou.main;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -8,15 +9,19 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.mingle.ZiYou.adapter.MyAdapter;
+import com.mingle.ZiYou.bean.Scene;
 import com.mingle.ZiYou.content.MapActivity;
 import com.mingle.ZiYou.content.TestMapActivity;
+import com.mingle.ZiYou.service.SceneInfor;
 import com.mingle.myapplication.R;
 
 import java.util.ArrayList;
@@ -25,10 +30,13 @@ import java.util.List;
 import java.util.Map;
 
 import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.listener.FindListener;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+private List<Scene> scenes=new ArrayList<Scene>();
+    private  ListView listView;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -41,15 +49,16 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        ListView listView=(ListView)findViewById(R.id.list);
+        listView=(ListView)findViewById(R.id.list);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
         setSupportActionBar(toolbar);
-
-        List<Map<String, Object>> list=getData();
+        getAllScenes();
+        Log.i("size:1",scenes.size()+"");
+        List<Map<String, Object>> list=getData(scenes);
         listView.setAdapter(new MyAdapter(this, list));
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -125,15 +134,61 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    public List<Map<String, Object>> getData(){
+    public List<Map<String, Object>> getData(List<Scene> scenes){
         List<Map<String, Object>> list=new ArrayList<Map<String,Object>>();
-        for (int i = 0; i < 10; i++) {
+        SceneInfor sceneInfor = new SceneInfor();
+//        List<Scene> scenes = sceneInfor.getSceneList(MainActivity.this);
+        for (int i = 0; i < scenes.size(); i++) {
             Map<String, Object> map=new HashMap<String, Object>();
             map.put("image", R.drawable.demo_list);
-            map.put("title", "title"+i);
+            map.put("title", scenes.get(i).getSname());
             map.put("info", "information"+i);
             list.add(map);
         }
         return list;
+    }
+
+    /*
+    获取数据
+     */
+
+    //获取景区列表
+    public void getAllScenes(){
+        BmobQuery<Scene> sceneBmobQuery=new BmobQuery<Scene>();
+        sceneBmobQuery.findObjects(MainActivity.this, new FindListener<Scene>() {
+            @Override
+            public void onSuccess(List<Scene> object) {
+                // TODO Auto-generated method stub
+                List<Scene> sceneList = new ArrayList<Scene>();
+                Toast.makeText(MainActivity.this,"查询成功：共"+object.size()+"条数据。",
+                        Toast.LENGTH_SHORT).show();
+                for (Scene scene : object) {
+                    sceneList.add(scene);
+                }
+//                getData(sceneList);
+                scenes=sceneList;
+                Log.i("size:2",scenes.size()+"");
+                List<Map<String, Object>> list=getData(scenes);
+                listView.setAdapter(new MyAdapter(MainActivity.this, list));
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        //Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                        //测试用，启动TestMapActivity
+                        Intent intent = new Intent(MainActivity.this, TestMapActivity.class);
+                        startActivity(intent);
+                    }
+                });
+
+            }
+
+            @Override
+            public void onError(int code, String msg) {
+                // TODO Auto-generated method stub
+                Toast.makeText(MainActivity.this,"查询失败："+msg,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+//        return sceneList;
     }
 }
